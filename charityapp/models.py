e0905771f9091f752af3facd80752f77c2469f7e
Vincent <chridtinewanja@gmail.com>
2020-08-25 15:34:07 +0300

    This adds the donor model for the project

--- charityapp/models.py
+++ charityapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Donor(models.Model):
+    name = models.CharField(max_length=100,blank=True)
+    email = models.EmailField(maxlength=254)
+    image= models.ImageField(blank=True)
+    contact = models.CharField(max_length=255)
+    def __str__(self):
+        return f'{self.name}'
+    def save_donor(self):
+        self.save()
+    def delete_donor(self):
+        self.delete()
