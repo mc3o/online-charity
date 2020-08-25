32234370bbc5034286ef59826c30f4cf9ebe0022
NgugiMuthoni <ngugimuthoni43@gmail.com>
2020-08-25 18:10:01 +0300

    Ngo model and the respective tests

--- ngo/models.py
+++ ngo/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
+# from pyuploadcare.dj.models import ImageField
+# from tinymce.models import HTMLField
+
 
 # Create your models here.
+
+class NGO(models.Model):
+    id = models.AutoField
+    name = models.CharField(max_length=50,)
+    location = models.CharField(max_length=50,)
+    case = models.CharField(max_length=100,)
+    phone = models.CharField(max_length=20, blank=False,)
+    email = models.CharField(max_length=30,)
+    image = models.ImageField(blank=True,)
+
+    def create_ngo(self):
+        self.save()
+
+    def delete_ngo(self):
+        self.delete()
+
+    @classmethod
+    def search_by_name(cls, id_):
+        ngo = cls.objects.filter(name=id)
--- ngo/tests.py
+++ ngo/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import NGO
+
 
 # Create your tests here.
+class NGOTestCase(TestCase):
+    def setUp(self):
+        self.new_ngo = NGO(name="orphanblack", location='kenya', case='orphanage donations', phone='+2547123456789',
+                           email='email@gmail.com')
+
+    def tearDown(self):
+        NGO.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_ngo, NGO))
+
+    def test_save_ngo(self):
+        self.new_ngo.create_ngo()
+        ngo = NGO.objects.all()
+        self.assertTrue(len(ngo) > 0)
+
+    def test_delete_ngo(self):
+        self.new_ngo.create_ngo()
+        self.new_ngo.delete_ngo()
+        ngo = NGO.objects.all()
+        self.assertEqual(len(ngo), 0)
