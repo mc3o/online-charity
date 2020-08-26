19207572aa30cc8929efa0af223c460dc35bcc41
Joan_E <joanevans18@gmail.com>
2020-08-26 09:25:08 +0300

    User Authentication - user can signup and receives an email confirming registration - User can reset his/her password - user can view his/her profile and edit/update his/her info

--- charityapp/views.py
+++ charityapp/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from .forms import SignUpForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+from .emails import send_welcome_email
 
 # Create your views here.
+def index(request):
+    return render(request, 'users/welcome.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            email = form.cleaned_data.get('email')
+            user = authenticate(username=username, password=raw_password)
+            messages.success(request, f'Account created')
+            send_welcome_email(username, email)
+            login(request, user)
+            return redirect('welcome')
+    else:
+        form = SignUpForm()
+    return render(request, 'users/signup.html', {'form': form})
+
+@login_required
+def profile(request):
+    user = request.user
+    if request.method == 'POST':
+        up_form = UserUpdateForm(request.POST, instance=request.user)
+        pr_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if up_form.is_valid() and pr_form.is_valid():
+            up_form.save()
+            pr_form.save()
+            messages.success(request, f'Account has been updated')
+            return redirect('profile')
+    else:
+        up_form = UserUpdateForm(instance=request.user)
+        pr_form = ProfileUpdateForm(instance=request.user.profile)
+    content = {
+        'user_form': up_form,
+        'profile_form': pr_form,
+    }
+    return render(request, 'users/profile.html', content)
