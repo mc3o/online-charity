f565736b38e780a0ff4af763019e4116cfeae76b
Mathews Alufwani <mathewalufwani@gmail.com>
2020-08-31 08:42:52 +0300

    Polish the User Inteface

--- charity/settings.py
+++ charity/settings.py
@@ -151,7 +151,7 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static"), 
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
