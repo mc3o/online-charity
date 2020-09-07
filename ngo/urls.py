4ce3007e17f36ecc88209516fffb96de052e60db
NgugiMuthoni <59652484+NgugiMuthoni@users.noreply.github.com>
2020-09-07 15:25:08 +0300

    origin

--- ngo/urls.py
+++ ngo/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.welcome, name='welcome'),
+    path('', views.landing, name='landing'),
+    path('welcome/', views.welcome, name='welcome'),
     path('admin-profile/', views.admin_profile, name='admin_profile'),
     path('request-donation/', views.request_donation, name='request_donation'),
     path('donation/<int:id>/request-approve/', views.approve_request, name='approve_request'),
